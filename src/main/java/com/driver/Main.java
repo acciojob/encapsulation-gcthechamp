6b0b2eb067099dc425897001171b5cccad610308
gcthechamp <gopalsatyam35@gmail.com>
2023-07-12 00:31:05 +0530

    Assignment 3 Retry

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,8 +6,8 @@ public class Main {
     {
         RWOnly obj = new RWOnly();
 
-        obj.name = "Gopal";
-        System.out.println(obj.name);
+        //obj.name = "Gopal";
+        //System.out.println(obj.name);
 
         //Error: name has private access in com.driver.RWOnly
 
