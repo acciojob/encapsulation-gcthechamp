941a1547d80891ac7aa2dccd930d5b290254cf70
gcthechamp <gopalsatyam35@gmail.com>
2023-07-12 00:27:27 +0530

    Assignment 3 done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args)
+    {
+        RWOnly obj = new RWOnly();
+
+        obj.name = "Gopal";
+        System.out.println(obj.name);
+
+        //Error: name has private access in com.driver.RWOnly
+
+        obj.setName("Gopal");
+
+        System.out.println(obj.getName());
+    }
   
 }
